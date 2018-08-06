082a20e827ebde947dd5325d22249cd70fa7331e
Jordan Kohl <kohl.jordan@gmail.com>
2018-08-06 14:57:27 -0700

    roughed in gist library

---
+++ project/src/library.js
@@ -0,0 +1,21 @@
+// GitHub's API is rate limited to 60 requests per hour for anonymous users. For that reason, you can use our proxy if needed.
+const url = 'https://api.github.com';
+const urlProxy = 'https://zapier-frontend-test.herokuapp.com/api/github';
+
+const handleError = (err) => console.log(err);
+const handleSuccess = (response) => response.json();
+
+export default {
+  // should provide a function that, given a username, retrieves the public gists for that particular user.
+  getPublicGistsByUsername: (username) => {
+    return fetch(`${url}/users/${username}/gists`)
+      .then(handleSuccess)
+      .catch(handleError);
+  },
+  // should provide a function that, given a gist ID, retrieves a specific gist.
+  getGistById: (id) => {
+    return fetch(`${url}/gists/${id}`)
+      .then(handleSuccess)
+      .catch(handleError);
+  }
+}
